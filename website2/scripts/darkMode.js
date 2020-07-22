ecc31083dfa6023857359b130ce9793c36279b7d
instagr4h4m <gh1315@nyu.edu>
2020-07-22 17:44:53 -0400

    Added second website concepts
    The directory "website2" contains documents for a more minimal web design which is still a work in progress, most of it is identical to the original

---
+++ website2/scripts/darkMode.js
@@ -0,0 +1,46 @@
+// Graham Harris
+// darkMode.js - converts between dark and light modes
+
+// Code adapted from
+// https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
+
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+var darkOn;
+var sections = ["main", "header", "buttons", "footer", "footer2", "linksAndDocs", "info", "line"]
+
+function changeTheme() {
+    // For every section that has dark mode enabled, add/remove "dark-theme"
+    for (i=0; i<sections.length; i++) {
+        document.getElementById(sections[i]).classList.toggle("dark-theme");
+    }
+}
+
+// Method that toggles dark mode
+function toggleDark() {
+
+    // If the user already prefers dark mode:
+    if (prefersDark.matches) {
+        // "Body" is treated differently because its an HTML tag and not a class
+        document.body.classList.toggle("dark-theme");
+        // Change the theme of class elements
+        changeTheme();
+        darkOn = false;
+    } 
+    else if (darkOn) {
+        // "Body" is treated differently because its an HTML tag and not a class
+        document.body.classList.toggle("dark-theme");
+        // Change the theme of class elements
+        changeTheme();
+        darkOn = false;
+    }
+    // If the user doesn't prefer dark mode or toggles it off:
+    else {
+        // Same logic as above
+        document.body.classList.toggle("dark-theme");
+        changeTheme();
+        darkOn = true;
+    }
+}
+
+// Instantiate the method so that there's an initial toggle on website load
+toggleDark();
