0be24be3d128741d6a732fe2db1bafa58accd942
instagr4h4m <grahamh1019@gmail.com>
2021-10-04 17:36:15 -0400

    Efficiency change
    Buttons now called by int instead of hardcoded name

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,13 +1,9 @@
 var a = document.getElementById("aboutMe");
 var b = document.getElementById("workExp");
 var c = document.getElementById("school");
-var d = document.getElementById("certifications");
-var e = document.getElementById("linksAndDocs");
-var f = document.getElementById("title");
+var d = document.getElementById("projects");
 
-var arr = [a,b,c,d,e,f];
-
-var index = 0;
+var arr = [a,b,c,d];
 
 function loop(index) {
 	for (i=0; i<arr.length;i++) {
@@ -21,28 +17,7 @@ function loop(index) {
 }
 
 // Main section button displays
-function aboutMe() {
-	loop(0);
-}
-
-function workExp() {
-	loop(1);
-}
-
-function school() {
-	loop(2);
-}
-
-function certifications() {
-	loop(3);
-}
-
-function linksAndDocs() {
-	loop(4);
-}
-
-// Not in use
-function title() {
-	loop(5);
+function press(x) {
+	loop(x);
 }
 
